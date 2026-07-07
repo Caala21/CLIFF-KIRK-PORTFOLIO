95b771b3cd3674a86c035130465a3e8af740d7c9
Caala21 <kirkcliff06@gmail.com>
2026-07-07 13:40:52 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Projects from "./components/Projects";
 import About    from "./components/About";
 import Contact  from "./components/Contact";
 import Footer   from "./components/Footer";
+import AutomationSection from './components/AutomationSection'
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Hero />
         <Projects />
         <About />
+        <AutomationSection />
         <Contact />
       </main>
       <Footer />
